2d9169f7fb6bc4c75089337f9480631523c685f1
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-08 14:03:13 -0400

    Update Index.js

--- server/node_moudles/Index.js
+++ server/node_moudles/Index.js
@@ -2,6 +2,6 @@
  
  var server = http.createServer(function(req, res) {
  res.writeHead(200);
- res.end(Helllo! Welcome to Mountio-Database Simple HTTP static Web Genartor Made by Github user Trafort');
+ res.end('Hello! Welcome to Mountio-Database Simple HTTP static Web Genartor Made by Github user Trafort');
  });
  server.listen(8082);
